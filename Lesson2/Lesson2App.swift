bc8b0d9c5c27229bd8f388437c44b7959a214b93
selimolmaz <selimolmaz3.1415@gmail.com>
2023-03-15 17:41:15 +0300

    Initial Commit

---
+++ Lesson2/Lesson2App.swift
@@ -0,0 +1,17 @@
+//
+//  Lesson2App.swift
+//  Lesson2
+//
+//  Created by Selim Olmaz on 15.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct Lesson2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
