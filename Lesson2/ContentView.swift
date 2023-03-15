5e5a9c6bb06fea187b8df301be53ac5ecd9a9ab9
selimolmaz <selimolmaz3.1415@gmail.com>
2023-03-15 19:22:33 +0300

    First commit for lesson2

--- Lesson2/ContentView.swift
+++ Lesson2/ContentView.swift
@@ -9,13 +9,56 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        
+        ZStack {
+            Color(.systemRed).ignoresSafeArea()
+            
+            //alt + cmd and <- is fold code snippets
+            VStack(alignment: .leading, spacing: 20.0) {
+                
+                Image("monke")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .cornerRadius(15)
+                
+                HStack {
+                    Text("Monke")
+                        .font(.title)
+                        .fontWeight(.bold)
+                    
+                    Spacer()
+                    // Rate and Reviews
+                    VStack {
+                        HStack{
+                            //SFsymbols 4 for images and symbols
+                            Image(systemName: "star.fill")
+                            Image(systemName: "star.fill")
+                            Image(systemName: "star.fill")
+                            Image(systemName: "star.leadinghalf.fill")
+                            Image(systemName: "star")
+                        }
+                        //Reviews
+                        Text("(Reviews: 234)")
+                    }.foregroundColor(.orange)
+                        .font(.caption)
+                    }
+                // Cover Letter
+                Text("Come visit the monke with some snacks. İt is gonna happy with that :3")
+                // symbols
+                HStack{
+                    Spacer()
+                    Image(systemName: "binoculars")
+                    Image(systemName: "fork.knife")
+                } .foregroundColor(.gray)
+                    .font(.caption)
+                
+            }
+            .padding()
+            .background(Rectangle().foregroundColor(.white).cornerRadius(15).shadow(radius: 15))
+            // if there is a second padding elements its automaticly padding outside
+            .padding(15)
         }
-        .padding()
+        
     }
 }
 
